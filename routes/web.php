8fe97d55f1edca03b041faca76fd36ea92b2d4d6
unknown <yaar81@gmail.com>
2024-05-20 21:16:50 +0700

    crud dasar gedung

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\GedungController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [GedungController::class, 'index'])->name('user.index');
+Route::get('/create-gedung',[GedungController::class,'create'])->name('user.create-gedung');
+Route::post('/store-gedung',[GedungController::class,'store'])->name('user.store-gedung');
+Route::get('/edit-gedung/{id}',[GedungController::class,'edit'])->name('user.edit-gedung');
+Route::post('/update-gedung/{id}',[GedungController::class,'update'])->name('user.update-gedung');
+Route::post('/delete-gedung/{id}',[GedungController::class,'delete'])->name('user.delete-gedung');
