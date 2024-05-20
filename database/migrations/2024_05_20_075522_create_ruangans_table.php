8afd7c97c6feb04886812fac7c448a96d510a5a3
unknown <yaar81@gmail.com>
2024-05-20 15:34:23 +0700

    first commit

---
+++ database/migrations/2024_05_20_075522_create_ruangans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ruangan', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_gedung')->constrained('gedung'); // Define foreign key constraint
+            $table->string('nama_ruangan');
+            $table->enum('jenis', [0, 1, 2]);
+            $table->integer('kapasitas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ruangans');
+    }
+};
---
+++ database/migrations/2024_05_20_080821_create_permintaans_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('permintaan', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_ruangan')->constrained('ruangan');
+            $table->foreignId('id_user')->constrained('user');
+            $table->enum('level',[0,1]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('permintaans');
+    }
+};
