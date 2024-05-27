f59905ea8b6cdd8b330865b89fbbd70e605d4bd2
unknown <yaar81@gmail.com>
2024-05-27 21:56:16 +0700

    arya, feature/sidebar-user

--- resources/views/components/organism/sidebar.blade.php
+++ resources/views/components/organism/sidebar.blade.php
@@ -1,22 +1,23 @@
 <div class="rectangle-parent">
-    {{-- <div class="group-child">
-    </div>
-    <div class="group-item">
-    </div> --}}
-    <div class="dashboard">
-    <div class="riwayat-pemesanan">Dashboard</div>
-    <img class="vector-icon2" alt="" src="{{asset('assets/img/dashboard.svg')}}">
-    </div>
-    <div class="request">
-        <div class="riwayat-pemesanan">Request Ruangan</div>
-        <img class="dooropenfill-icon" alt="" src="{{asset('assets/img/request.svg')}}">
-    </div>
-    <div class="riwayat">
-        <div class="riwayat-pemesanan">Riwayat Pemesanan</div>
-        <img class="vector-icon1" alt="" src="{{asset('assets/img/riwayat.svg')}}">
-    </div>
-    <div class="user-parent">
-    <div class="user">User</div>
-    <img class="vector-icon" alt="" src="{{asset('assets/img/user.svg')}}">
-    </div>
+        <div class="Judul">
+            <div class="teks-judul">Peminjaman Ruangan FMIPA</div>
+        </div>
+        <div class="group-item">
+            <div class="dashboard">
+                <img class="vector-icon" alt="" src="{{asset('assets/img/dashboard.svg')}}">
+                <div class="teks-label">Dashboard</div>
+            </div>
+        </div>
+        <div class="request">
+            <img class="vector-icon" alt="" src="{{asset('assets/img/request.svg')}}">
+            <div class="teks-label">Request Ruangan</div>
+        </div>
+        <div class="riwayat">
+            <img class="vector-icon" alt="" src="{{asset('assets/img/riwayat.svg')}}">
+            <div class="teks-label">Riwayat Pemesanan</div>
+        </div>  
+        <div class="user-parent">
+            <img class="vector-icon" alt="" src="{{asset('assets/img/user.svg')}}">
+            <div class="user">User</div>
+        </div>
 </div>
